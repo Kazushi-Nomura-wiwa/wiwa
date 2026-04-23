cb548a62a63a5d6edcad2e7df72e7d52740550a1
Kazushi-Nomura-wiwa <kazushi_nomura@wiwa.jp>
2026-04-23 11:22:00 +0900

    response修正

--- wiwa/controllers/admin/assist.py
+++ wiwa/controllers/admin/assist.py
@@ -1,6 +1,6 @@
 # パスとファイル名: wiwa/controllers/admin/assist.py
 from wiwa.core.renderer import TemplateRenderer
-from wiwa.core.response import Response
+from wiwa.core.response import html
 from wiwa.core.route_scanner import RouteScanner
 
 
@@ -21,8 +21,4 @@ def routes(request, route=None):
         request=request,
     )
 
-    return Response(
-        body=body,
-        status="200 OK",
-        headers=[("Content-Type", "text/html; charset=utf-8")],
-    )
+    return html(body)
